5d039d902ac269eb2d335f61a63b640893ffd1f5
kmj685 <kmj202005@naver.com>
2026-02-02 11:33:38 +0900

    무비 리스트 Day Week 각각 불러오고 버튼으로 노출시키는 리스트 바꾸기

--- app/src/main/java/com/dapaeng12/ruachmovie/features/movieList/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/dapaeng12/ruachmovie/features/movieList/presentation/activities/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.dapaeng12.ruachmovie.features.movieList.presentation.activities
 
+import android.content.res.ColorStateList
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.TextViewCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dapaeng12.ruachmovie.R
@@ -20,10 +26,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var movieAdapter: MovieRecycleAdapter
     private val viewModel : MovieListViewModel by viewModels{
-        MovieListViewModelFactory(GetTrendingDayMoviesUseCase(MovieListRepositoryImpl()))
+        MovieListViewModelFactory(MovieListRepositoryImpl())
     }
 
     private lateinit var movieListRecyclerView: RecyclerView
+    private lateinit var dayButton : Button
+    private lateinit var weekButton : Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +41,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         movieListRecyclerView = findViewById(R.id.movie_list_recycler_view)
+        dayButton = findViewById(R.id.day_button)
+        weekButton = findViewById(R.id.week_button)
+
+        dayButton.setOnClickListener { clickDayBtn() }
+        weekButton.setOnClickListener { clickWeekBtn() }
         viewModel.movieList.observe(this) { list ->
             movieAdapter.submitList(list)
         }
@@ -47,4 +61,42 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+    fun clickDayBtn() {
+        if (viewModel.listType == "day") {
+            return
+        } else {
+            viewModel.selectDay()
+            selectBtn(dayButton)
+            unSelectBtn(weekButton)
+        }
+    }
+
+    fun clickWeekBtn() {
+        if (viewModel.listType == "week") {
+            return
+        } else {
+            viewModel.selectWeek()
+            selectBtn(weekButton)
+            unSelectBtn(dayButton)
+        }
+    }
+
+    fun selectBtn(button: Button) {
+        val btn = button
+        btn.backgroundTintList = ColorStateList.valueOf(
+            ContextCompat.getColor(this, R.color.selectBtnBg)
+        )
+        TextViewCompat.setTextAppearance(btn, R.style.BtnText_Bold)
+        btn.setTextColor(ContextCompat.getColor(this, android.R.color.white))
+    }
+
+    fun unSelectBtn(button: Button) {
+        val btn = button
+        btn.backgroundTintList = ColorStateList.valueOf(
+            ContextCompat.getColor(this, R.color.unSelectBtnBg)
+        )
+        TextViewCompat.setTextAppearance(btn, R.style.BtnText_Normal)
+        btn.setTextColor(ContextCompat.getColor(this, android.R.color.black))
+    }
+
 }
--- app/src/main/java/com/dapaeng12/ruachmovie/features/movieList/presentation/viewmodels/MovieListViewModel.kt
+++ app/src/main/java/com/dapaeng12/ruachmovie/features/movieList/presentation/viewmodels/MovieListViewModel.kt
@@ -9,35 +9,78 @@ import com.dapaeng12.ruachmovie.features.movieList.data.datasources.RetrofitMana
 import com.dapaeng12.ruachmovie.features.movieList.domian.entities.TmdbMovie
 import com.dapaeng12.ruachmovie.features.movieList.domian.repositories.MovieListRepository
 import com.dapaeng12.ruachmovie.features.movieList.domian.usecases.GetTrendingDayMoviesUseCase
+import com.dapaeng12.ruachmovie.features.movieList.domian.usecases.GetTrendingWeekMoviesUseCase
 import kotlinx.coroutines.launch
 
 class MovieListViewModel(
-    private val getTrendingDayMoviesUseCase: GetTrendingDayMoviesUseCase
+    private val repository: MovieListRepository
+
 ) : ViewModel() {
+    private val DAY : String = "day"
+    private val WEEK : String = "week"
+
     val TAG : String = "로그"
+    var listType : String = ""
     private val _movieList = MutableLiveData<List<TmdbMovie>>()   // 내부에서만 수정
     val movieList: LiveData<List<TmdbMovie>> = _movieList
 
+    var dayMovieList: List<TmdbMovie> = listOf()
+    var weekMovieList: List<TmdbMovie> = listOf()
+
+    private val getTrendingDayMoviesUseCase = GetTrendingDayMoviesUseCase(repository)
+    private val getTrendingWeekMoviesUseCase = GetTrendingWeekMoviesUseCase(repository)
     init {
-        loadDay()
+        loadMovieList()
     }
 
     fun loadDay() {
         viewModelScope.launch {
             val response = getTrendingDayMoviesUseCase()
             _movieList.value = response
+            listType = DAY
         }
+    }
+
+    fun loadWeek() {
+        viewModelScope.launch {
+            val response = getTrendingWeekMoviesUseCase()
+            _movieList.value = response
+            listType = WEEK
+        }
+    }
+
+    fun loadMovieList() {
+        viewModelScope.launch {
+            val dayResponse = getTrendingDayMoviesUseCase()
+            dayMovieList = dayResponse
+
+            val weekResponse = getTrendingWeekMoviesUseCase()
+            weekMovieList = weekResponse
+
+            _movieList.value = dayMovieList
+            listType = DAY
+        }
+    }
+
+    fun selectDay() {
+        _movieList.value = dayMovieList
+        listType = DAY
+    }
+
+    fun selectWeek() {
+        _movieList.value = weekMovieList
+        listType = WEEK
     }
 }
 
 class MovieListViewModelFactory(
-    private val getTrendingDayMoviesUseCase: GetTrendingDayMoviesUseCase
+    private val repository: MovieListRepository
 ) : androidx.lifecycle.ViewModelProvider.Factory {
 
     override fun <T : androidx.lifecycle.ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MovieListViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return MovieListViewModel(getTrendingDayMoviesUseCase) as T
+            return MovieListViewModel(repository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
